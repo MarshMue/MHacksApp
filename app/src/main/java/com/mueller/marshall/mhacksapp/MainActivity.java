92fa4fefd2882001dc31867a1e47da179a71f97f
Flaxbeard <Flaxbeard@users.noreply.github.com>
2014-09-06 14:33:10 -0500

    Update MainActivity.java
    Now the code doesn't look like trash

--- app/src/main/java/com/mueller/marshall/mhacksapp/MainActivity.java
+++ app/src/main/java/com/mueller/marshall/mhacksapp/MainActivity.java
@@ -39,75 +39,82 @@ public class MainActivity extends Activity {
             "w",
             "x",
             "z"};
+            
         private static String getRandCons() {
             return mConsonants[(int) (Math.random() * 20)];
         }
+        
         //define vowels
         private static String[] mvowels = new String[] {
-                "a",
-                "e",
-                "i",
-                "o",
-                "u",
-                "y"};
-            private static String getRandVow() {
-                return mvowels[(int) (Math.random() * 6)];
-            }
+            "a",
+            "e",
+            "i",
+            "o",
+            "u",
+            "y"};
+                
+        private static String getRandVow() {
+            return mvowels[(int) (Math.random() * 6)];
         }
+    }
 
-    private void generateName() { //this is where the name gets created
+    //this is where the name gets created
+    private void generateName() { 
         mGeneratedName = (TextView) findViewById(R.id.randName);
         String name = new String();
-        if (index == 0){
-        //index number correlates to which name algorithm is used
-            //including index numbers for debug purposes
-        name = JavaStringArrayTest.getRandCons().toUpperCase() + JavaStringArrayTest.getRandVow()
-        + JavaStringArrayTest.getRandCons() + JavaStringArrayTest.getRandCons()
-        + JavaStringArrayTest.getRandVow() + JavaStringArrayTest.getRandCons() + " 0";
-         //set the name to the TextView
-        } else if (index == 1) { //there's a better way of doing this, but I cant remember atm
-            name = JavaStringArrayTest.getRandVow().toUpperCase() + JavaStringArrayTest.getRandCons()
-                    + JavaStringArrayTest.getRandCons() + JavaStringArrayTest.getRandVow() + " 1";
+        switch (index) {
+            case 0:
+                //index number correlates to which name algorithm is used, including index numbers for debug purposes
+                
+                //set the name to the TextView
+                name = JavaStringArrayTest.getRandCons().toUpperCase() + JavaStringArrayTest.getRandVow()
+                    + JavaStringArrayTest.getRandCons() + JavaStringArrayTest.getRandCons()
+                    + JavaStringArrayTest.getRandVow() + JavaStringArrayTest.getRandCons() + " 0";
+                break;
+            case 1:
+                name = JavaStringArrayTest.getRandVow().toUpperCase() + JavaStringArrayTest.getRandCons()
+                     + JavaStringArrayTest.getRandCons() + JavaStringArrayTest.getRandVow() + " 1";
+                break;
         }
         mGeneratedName.setText(name);
     }
 
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            mGenerateButton = (Button)findViewById(R.id.buttonGenerate);
-            mGenerateButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    generateName();
-                    index = (int) (Math.random() * NUM_OF_NAME_ALGS);
-                }
-            });
+        mGenerateButton = (Button)findViewById(R.id.buttonGenerate);
+        mGenerateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateName();
+                index = (int) (Math.random() * NUM_OF_NAME_ALGS);
+            }
+        });
 
     }
 
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.main, menu);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
             return true;
         }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-            if (id == R.id.action_settings) {
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
+        return super.onOptionsItemSelected(item);
     }
+}
 
